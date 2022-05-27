d2fe8291f581576b2c1f8e75702cbe40b156edfc
Will <chavezdiaz4@gmail.com>
2022-05-27 12:03:17 -0500

    config cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import administradorRoutes from './routes/administrador.routes'
 import proveedoresRoutes from './routes/proveedores.routes'
 
@@ -11,6 +12,7 @@ app.set('port', process.env.PORT || 5000);
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 app.use(morgan('dev'));
 
 
